76e97757a19bf91b0de35912e3359f805d05e493
esteranodin <esteranodin@hotmail.fr>
2024-11-28 11:59:06 +0100

    DONE : SETUP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+     "./index.html"
+  ],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          'soft-orange': 'hsl(35, 77%, 62%)',
+          'soft-red': 'hsl(5, 85%, 63%)',
+        },
+        neutral: {
+          'off-white': 'hsl(36, 100%, 99%)',
+          'grayish-blue': 'hsl(233, 8%, 79%)',
+          'dark-grayish-blue': 'hsl(236, 13%, 42%)',
+          'very-dark-blue': 'hsl(240, 100%, 5%)',
+        },
+      },
+      fontFamily: {
+        body: ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        paragraph: '15px',
+      },
+    },
   },
   plugins: [],
 }
