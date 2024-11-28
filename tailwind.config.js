194e7353f80fd3c73b5a8e44017da1bfd574d10b
esteranodin <esteranodin@hotmail.fr>
2024-11-28 15:11:02 +0100

    DONE : config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,13 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          'soft-orange': 'hsl(35, 77%, 62%)',
-          'soft-red': 'hsl(5, 85%, 63%)',
+          'dark-blue': 'hsl(219, 37, 17)',
+          'blue': 'hsl(212, 29, 36)',
+          'grey': 'hsl(214, 23, 56)',
         },
         neutral: {
-          'off-white': 'hsl(36, 100%, 99%)',
-          'grayish-blue': 'hsl(233, 8%, 79%)',
-          'dark-grayish-blue': 'hsl(236, 13%, 42%)',
-          'very-dark-blue': 'hsl(240, 100%, 5%)',
+          'off-white': 'hsl(75, 6, 87)',
+          'very-dark-blue': 'hsl(211, 92, 5)',
         },
       },
       fontFamily: {
